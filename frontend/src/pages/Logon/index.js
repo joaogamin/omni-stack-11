b6e7ae15c069db94b3ca5cdee6ce4665843a6a8a
João Pedro Gamin <joaumxp@hotmail.com>
2020-03-25 15:35:44 -0300

    conclusao logon page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -12,7 +12,7 @@ export default function Logon(){
                 <form>
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
                     <a href="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro  
