db9055f923381330b9b9d8c32a766c3525bafb5c
João Pedro Gamin <joaumxp@hotmail.com>
2020-03-25 21:19:47 -0300

    layout pagina do perfil

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes(){
 
@@ -11,6 +12,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Logon}/>
                 <Route path="/cadastro" component={Register}/>
+                <Route path="/perfil" component={Profile}/>
             </Switch>   
         </BrowserRouter>
     );
