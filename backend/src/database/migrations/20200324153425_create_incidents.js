89a256a16c4ad3769b35124070358673cdf4e8ca
João Pedro Gamin <joaumxp@hotmail.com>
2020-03-30 16:28:48 -0300

    Format code backend

--- backend/src/database/migrations/20200324153425_create_incidents.js
+++ backend/src/database/migrations/20200324153425_create_incidents.js
@@ -1,6 +1,6 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('incidents', function(table){
+exports.up = function (knex) {
+    return knex.schema.createTable('incidents', function (table) {
         table.increments();
         table.string('title').notNullable();
         table.string('description').notNullable();
@@ -8,9 +8,9 @@ exports.up = function(knex) {
 
         table.string('ong_id').notNullable();
         table.foreign('ong_id').references('id').inTable('ongs');
-  });
+    });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('increments');
 };
